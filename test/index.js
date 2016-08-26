e8ae0ef4df7e6f9e39615d6c6a8e90c23686f9d1
Robin Bentley <robinbentley@me.com>
2016-08-26 18:41:13 +0100

    Rename testServer -> server

--- test/index.js
+++ test/index.js
@@ -1,14 +1,14 @@
 import Lab from 'lab';
 import Code from 'code';
 
-import testServer from './test-server';
+import server from './test-server';
 
 const lab = exports.lab = Lab.script();
 
 lab.test('the test server running and accepting injection', (done) => {
   let options = {url: '/'};
 
-  testServer.inject(options, (reply) => {
+  server.inject(options, (reply) => {
     Code.expect(reply.result).to.equal('Test Server Available');
     Code.expect(reply.statusCode).to.equal(200);
     done();
