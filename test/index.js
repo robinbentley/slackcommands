78831e6e8065490e2a092a8a3ff1dd4707cfcd58
Robin Bentley <robinbentley@me.com>
2016-08-29 15:37:19 +0100

    Register commands plugin with test server

--- test/index.js
+++ test/index.js
@@ -14,3 +14,54 @@ lab.test('the test server running and accepting injection', (done) => {
     done();
   });
 });
+
+lab.experiment('Testing: Urban', () => {
+  lab.test('It returns a random word', (done) => {
+    let options = {
+      method: 'POST',
+      url: '/urban',
+      payload: {
+        text: null
+      }
+    };
+
+    server.inject(options, (reply) => {
+      Code.expect(reply.statusCode).to.equal(200);
+      Code.expect(reply.payload).to.include(['text']);
+      done();
+    });
+  });
+
+  lab.test('It searches for a defined word', (done) => {
+    let options = {
+      method: 'POST',
+      url: '/urban',
+      payload: {
+        text: 'test'
+      }
+    };
+
+    server.inject(options, (reply) => {
+      Code.expect(reply.statusCode).to.equal(200);
+      Code.expect(reply.payload).to.include(['text']);
+      done();
+    });
+  });
+
+  lab.test('It responds when a word is not found', (done) => {
+    let options = {
+      method: 'POST',
+      url: '/urban',
+      payload: {
+        text: 'testasaasdasdasdasdadsasdasd'
+      }
+    };
+
+    server.inject(options, (reply) => {
+      Code.expect(reply.statusCode).to.equal(200);
+      Code.expect(reply.result.text).to.equal('Nothing found! Looks like *testasaasdasdasdasdadsasdasd* isn\'t real.');
+      done();
+    });
+  });
+
+});
